38c848137b6e2a2600a0110790362e91f2d5106e
Brianna Keune <briannakeune@gmail.com>
2019-11-04 17:58:00 -0600

    everything is rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import axios from "axios"
 
 // local imports
 import Nav from './components/Nav.js'
 import Content from './components/Content.js'
 import Footer from './components/Footer.js'
 
-
-// package to keep my api key a secret
-require('dotenv').config();
-
 function App() {
-  const nasaUrl = process.env.REACT_APP_API_URL || "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
-
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    axios.get(nasaUrl)
-      .then(res => setData(res.data))
-      .catch(err => console.log(err))
-  }, [])
-
   return (
     <div className="App">
       <Nav />
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 
 function Content() {
+  const nasaUrl =
+    "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+
+  const [nasaObj, setNasaObj] = useState();
+
+  useEffect(() => {
+    axios
+      .get(nasaUrl)
+      .then(res => {
+        console.log(res);
+        setNasaObj(res.data);
+      })
+      .catch(err => console.log(err));
+  }, [nasaUrl]);
+
+  if (nasaObj === undefined) {
+    return <h3>Loading...</h3>;
+  } else {
     return (
-        <div>
-            Content here
-        </div>
-    )
+      <div>
+        <h1>{nasaObj.title}</h1>
+      <img src={nasaObj.hdurl} alt={nasaObj.explanation} />
+      <p>
+        <span className="date">{nasaObj.date}</span>
+        <span className="copyright">{nasaObj.copyright}</span>
+      </p>
+      <p className="explanation">{nasaObj.explanation}</p>
+      </div>
+    );
+  }
 }
 
 export default Content;
